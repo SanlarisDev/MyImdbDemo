473f7da3c5a052fe87f940bc0f2eeba17245de2e
SanlarisDev <cynsb90@gmail.com>
2021-09-29 12:51:25 +0200

    [+] Page detail checkpoint

--- src/app/pages/movie/detail-movie/detail-movie.component.ts
+++ src/app/pages/movie/detail-movie/detail-movie.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-detail-movie',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-movie.component.scss']
 })
 export class DetailMovieComponent implements OnInit {
+  public movie!: Movie | null;
+  public loadingListMovies: boolean = true;
 
-  constructor() { }
+  constructor(private movieService: MovieService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    let actualId = this.route.snapshot.paramMap.get('id');
+
+    if(actualId){
+      this.movieService.getMovie(actualId).subscribe({
+        next: (_movie) => {this.movie = _movie},
+        error: () => {},
+        complete: () => {this.loadingListMovies = false}
+      });
+
+    } else {
+      this.movie = null;
+      this.loadingListMovies = false;
+    }
+
   }
 
 }
