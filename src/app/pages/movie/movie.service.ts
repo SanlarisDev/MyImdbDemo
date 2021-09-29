71c1e2e27a193e6cc294b3dcc2522a254a38e53e
SanlarisDev <cynsb90@gmail.com>
2021-09-29 16:55:08 +0200

    [-]delete retry

--- src/app/pages/movie/movie.service.ts
+++ src/app/pages/movie/movie.service.ts
@@ -18,7 +18,7 @@ export class MovieService {
    * @returns Observable<Movie[]>
    */
   getAllMovies(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(this._baseUrl).pipe(retry(3), catchError(this.handleError));
+    return this.http.get<Movie[]>(this._baseUrl).pipe(catchError(this.handleError));
   }
 
   /**
@@ -27,7 +27,7 @@ export class MovieService {
    * @returns Observable<Movie>
    */
   getMovie(id: string): Observable<Movie> {
-    return this.http.get<Movie>(this._baseUrl + '/' + id).pipe(retry(3), catchError(this.handleError));
+    return this.http.get<Movie>(this._baseUrl + '/' + id).pipe(catchError(this.handleError));
   }
 
   /**
@@ -36,7 +36,7 @@ export class MovieService {
    * @returns Observable<Movie>
    */
   createMovie(movie: Movie): Observable<Movie> {
-    return this.http.put<Movie>(this._baseUrl, movie).pipe(retry(3), catchError(this.handleError));
+    return this.http.put<Movie>(this._baseUrl, movie).pipe(catchError(this.handleError));
   }
 
   /**
@@ -45,7 +45,7 @@ export class MovieService {
    * @returns Observable<Movie>
    */
   setMovie(movie: Movie): Observable<Movie> {
-    return this.http.put<Movie>(this._baseUrl  + '/' + movie.id, movie).pipe(retry(3), catchError(this.handleError));
+    return this.http.put<Movie>(this._baseUrl  + '/' + movie.id, movie).pipe(catchError(this.handleError));
   }
 
   /**
@@ -55,12 +55,14 @@ export class MovieService {
    */
   handleError(error: HttpErrorResponse){
     if(error.status === 0) {
+      // TODO: translate
       console.error('And error ocurred: ', error.error);
     } else {
       console.error(`Backend returned code ${error.status}, body was`, error.error);
     }
 
     return throwError(
+      // TODO: translate
       'Something  bad happened; please try again later.');
   }
 
