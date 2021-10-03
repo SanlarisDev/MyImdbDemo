9eb7ae5c4a1f4784ad2ad319d0a7162bdf383983
SanlarisDev <cynsb90@gmail.com>
2021-10-03 16:41:48 +0200

    [*] Checkpoint editMovie

--- src/app/pages/movie/edit-movie/edit-movie.component.ts
+++ src/app/pages/movie/edit-movie/edit-movie.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ParseService } from 'src/app/shared/parse/parse.service';
+import { URLS } from 'src/app/shared/urls';
+import { Movie } from '../movie';
+import { MovieService } from '../services/movie.service';
+
+export enum modeEdit {Edit, New}
 
 @Component({
   selector: 'app-edit-movie',
@@ -7,9 +14,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditMovieComponent implements OnInit {
 
-  constructor() { }
+  public movie!: Movie | null;
+  public loadingMovie: boolean = true;
+  public urls = URLS;
+  private mode: modeEdit = modeEdit.Edit;
+
+  constructor(
+    private parseService: ParseService,
+    private route: ActivatedRoute,
+    private movieService: MovieService) { }
 
   ngOnInit(): void {
+
+
   }
 
 }
